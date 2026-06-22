c38863cae3fefdeddb0354d3a98d0ff78de52229
AnandaTris <adotriharis@gmail.com>
2026-06-22 17:20:19 +0800

    1833. Maximum Ice Cream Bars (medium)

--- 1833.py
+++ 1833.py
@@ -1,19 +1,9 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        max_cost = max(costs)
-        freq = [0] * (max_cost + 1)
-        for cost in costs:
-            freq[cost] += 1
-        
-        count = 0
-        for price in range(1, max_cost + 1):
-            if freq[price] == 0:
-                continue
-            if coins < price:
-                break
-            can_buy = min(freq[price], coins // price)
-            count += can_buy
-            coins -= can_buy * price
-        
+        costs.sort()
+        count = 0   
+        for i in range(len(costs)):
+            if coins >= costs[i]:
+                coins -= costs[i]
+                count +=1
         return count
-        
