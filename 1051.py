ef328f2f1a2f16b293f6d9ae6580abb6b7a89dd3
AnandaTris <adotriharis@gmail.com>
2026-06-27 04:18:09 +0800

    1051. Height Checker

---
+++ 1051.py
@@ -0,0 +1,9 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        res = 0
+        tai = sorted(heights)
+        for i in range(len(heights)):
+            if heights[i] != tai[i]:
+                res+=1
+        return res
+
